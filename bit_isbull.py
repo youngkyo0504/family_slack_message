19d2ffcd931ce22a193d21ba4e60a599e9571cae
kky <keumky2@gamil.com>
2021-08-16 16:12:57 +0900

    ADD [github action func]

---
+++ bit_isbull.py
@@ -0,0 +1,26 @@
+import os
+import pyupbit
+
+#api연결
+access = os.environ['UPBIT_ACCESS']
+secret = os.environ['UPBIT_SECRET']
+upbit = pyupbit.Upbit(access, secret)
+
+#상승장인지 구분하겠습니다.
+# 120일 60일 20일 5일 현재 순이라면 강한 상승장입니다.
+# 위 순서대로 배열될 수록 강한 상승장입니다.
+# 현재가격이 20일 평균 가격보다 낮다면 사지 않는 것을 추천합니다.
+# 현재가격이 120일 20일 60일 위에 있을 때 사는 것을 추천합니다.
+# 가격은 원단위입니다.
+
+def bull_market(ticker):
+    df = pyupbit.get_ohlcv(ticker,count=120)
+    close = df['close']
+    ma = {
+    '120일 평균' : close.mean(),
+    '20일 평균' : close.iloc[-21:-1].mean(),
+    '60일 평균' : close.iloc[-61:-1].mean(),
+    '5일 평균' : close.iloc[-6: -1].mean(),
+    '현재 가격': pyupbit.get_current_price(ticker)
+    }
+    return sorted(ma.items(), key = lambda  x: x[1])
---
+++ github_utils.py
@@ -0,0 +1,41 @@
+from github import Github
+from datetime import datetime
+from pytz import timezone
+from bit_isbull import bull_market
+
+def get_today():
+    seoul_timezone = timezone('Asia/Seoul')
+    today = datetime.now(seoul_timezone)
+    today_data = today.strftime("%Y년 %m월 %d일")
+    return today_data
+
+def get_title():
+    return get_today() + "비트코인 가격"
+
+def get_github_repo(access_token, repository_name):
+    """
+    github repo object를 얻는 함수
+    :param access_token: Github access token
+    :param repository_name: repo 이름
+    :return: repo object
+    """
+    g = Github(access_token)
+    repo = g.get_user().get_repo(repository_name)
+    return repo
+
+
+def upload_github_issue(repo, title, body):
+    """
+    해당 repo에 title 이름으로 issue를 생성하고, 내용을 body로 채우는 함수
+    :param repo: repo 이름
+    :param title: issue title
+    :param body: issue body
+    :return: None
+    """
+    repo.create_issue(title=title, body=body)
+
+def upload_bull_market_issue(access_token, repository_name):
+    repo = get_github_repo(access_token,repository_name)
+    title = get_title()
+    body = bull_market("KRW-BTC")
+    upload_github_issue(repo,title, body)
