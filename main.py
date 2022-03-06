f5ea9e1ae95449c70df9e7f06fbcb4e90e138629
keumk <keumky2@gmail.com>
2022-03-06 20:07:11 +0900

    :green_heart:  알람으로 채널 고침

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ access_token = os.environ['MY_GITHUB_TOKEN']
 slack_token = os.environ['SLACK_TOKEN']
 body = bull_market("KRW-BTC")
 upload_bull_market_issue(access_token,"is_bull_market",body)
-post_message(slack_token,'#개발',body)
+post_message(slack_token,'#알람',body)
 
 
