f05e5d71dceabe404fcda15c4c948b6604ef3c24
keumky2 <keumky2@woowahan.com>
2025-01-11 12:49:44 +0900

    feat(google_sheets_manager): 환율 정보를 슬랙 메시지에 추가

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ slack_token = os.environ["SLACK_TOKEN"]
 body = bull_market("KRW-BTC")
 tether_premium_message = get_tether_premium()
 exchange_rate = read_exchange_rate()
-post_message(slack_token, "#알람", body + "\n" + tether_premium_message)
+post_message(slack_token, "#알람", body + "\n" + tether_premium_message + '\n' + '환율은 :' exchange_rate)
